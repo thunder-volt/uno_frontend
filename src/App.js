c6a80dbbe7f9d4582df09e98f6ae78871198adff
thunder-volt <ishu114407@gmail.com>
2023-05-25 13:12:45 +0530

    for ci cd

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Homepage from './components/Homepage'
+import Game from './components/Game'
+
+const App = () => {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path='/play' element={<Game />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
