7a6c9b92619f3d74b034435263cf5567d2080180
thunder-volt <ishu114407@gmail.com>
2023-05-25 16:50:29 +0530

    for ci cd

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom';
 import Homepage from './components/Homepage'
 import Game from './components/Game'
 
